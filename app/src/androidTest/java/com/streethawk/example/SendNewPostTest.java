7a850d1ea034104b1b92608f83baf9af547e4b3b
Ercan PINAR <ercanpinarr@gmail.com>
2017-02-05 14:14:25 +1000

    SendNewPostTest and ShowPostListTest added. (UnitTest)

---
+++ app/src/androidTest/java/com/streethawk/example/SendNewPostTest.java
@@ -0,0 +1,49 @@
+package com.streethawk.example;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.streethawk.example.activity.MainActivity;
+import com.streethawk.example.activity.SplashActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * SendNewPostTest , which will execute on an Android device.
+ *
+ *  Device Android Version >= 5.0  (Since it has a DataBinding structure.)
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SendNewPostTest {
+
+    @Rule
+    public ActivityTestRule<SplashActivity> mSplashActivityRule = new ActivityTestRule<>(
+            SplashActivity.class);
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        onView(withId(R.id.formEditTextTitle))
+                .perform(typeText("Test Title"), closeSoftKeyboard());
+        onView(withId(R.id.formEditTextBody))
+                .perform(typeText("Test Body"), closeSoftKeyboard());
+    }
+
+    @Test
+    public void sendNewPost() throws Exception {
+        onView(withId(R.id.buttonSend)).perform(click());
+    }
+}
